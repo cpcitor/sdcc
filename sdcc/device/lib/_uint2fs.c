2e33f52d69fd7cbb3670fe32f047c9ac187b7b8b
(no author) <(no author)@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2000-11-21 17:12:31 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.6.2'.
    git-svn-id: svn://svn.code.sf.net/p/sdcc/code/branches/unlabeled-1.6.2@510 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/device/lib/_uint2fs.c
+++
@@ -1,7 +0,0 @@
-#include <_float.h>
-
-/* convert unsigned int to float */
-float __uint2fs (unsigned int ui) {
-  unsigned long ul=ui;
-  return __ulong2fs(ul);
-}
--- sdcc/packihx/clean.mk
+++
@@ -1,7 +0,0 @@
-clean:
-	rm -f *core *[%~] *.[oa] *.output
-	rm -f .[a-z]*~ \#*
-	rm -f packihx
-
-distclean: clean
-	rm -f config.*

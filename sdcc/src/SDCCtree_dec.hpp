9c3189aa39c19d8041461a64ea15177f5da587cc
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2017-11-13 07:36:19 +0000

    Document choice of algorithm for tree-decomposition.
    git-svn-id: svn://svn.code.sf.net/p/sdcc/code/branches/nothorup@10180 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/SDCCtree_dec.hpp
+++ sdcc/src/SDCCtree_dec.hpp
@@ -604,11 +604,11 @@ void copy_undir(G1_t &G1, G2_t const &G2){
 
 #endif
 
-#undef USE_THORUP
-#undef USE_PP_FI_TM
-#undef USE_EX17
-#undef USE_PP_MD
-#define USE_PP_FI 1
+#undef USE_THORUP // Thorup's classic approach (default in SDCC pre-3.7.0). Substantially worse width than the otehr approaches.
+#define USE_PP_FI_TM 1 // A good trade-off between width and runtime
+#undef USE_EX17 // Slightly better width width than PP_FI_TM, but no polynomial runtime bound.
+#undef USE_PP_MD // Slightly worse width than PP_FI_TM.
+#undef USE_PP_FI // Slightly worse width than PP_FI_TM.
 
 // Get a nice tree decomposition for a cfg.
 template <class T_t, class G_t>

b4f8470d908044ef35e24f31029f5716c2a6f855
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2018-08-26 16:44:52 +0000

    Preparations for extracting CFGs for comparison
    git-svn-id: svn://svn.code.sf.net/p/sdcc/code/branches/lospre-vs-mcpre@10527 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/SDCClospre.cc
+++ sdcc/src/SDCClospre.cc
@@ -158,13 +158,36 @@ get_candidate_set(std::set<int> *c, const iCode *const sic, int lkey)
 }
 
 static bool
-setup_cfg_for_expression (cfg_lospre_t *const cfg, const iCode *const eic)
+invalidates_expression(const iCode *const eic, const iCode *const iic)
 {
-  typedef boost::graph_traits<cfg_lospre_t>::vertex_descriptor vertex_t;
   const operand *const eleft = IC_LEFT (eic);
   const operand *const eright = IC_RIGHT (eic);
   const bool uses_global = (eic->op == GET_VALUE_AT_ADDRESS || isOperandGlobal (eleft) || isOperandGlobal (eright) || IS_SYMOP (eleft) && OP_SYMBOL_CONST (eleft)->addrtaken || IS_SYMOP (eright) && OP_SYMBOL_CONST (eright)->addrtaken);
   const bool uses_volatile = POINTER_GET (eic) && IS_VOLATILE (operandType (eleft)->next) || IS_OP_VOLATILE (eleft) || IS_OP_VOLATILE (eright);
+
+  const operand *const left = IC_LEFT (iic);
+  const operand *const right = IC_RIGHT (iic);
+  const operand *const result = IC_RESULT (iic);
+
+  if (IC_RESULT (iic) && !IS_OP_LITERAL (result) && !POINTER_SET(iic) &&
+    (eleft && isOperandEqual (eleft, result) || eright && isOperandEqual (eright, result)))
+    return(true);
+  if (iic->op == FUNCTION || iic->op == ENDFUNCTION || iic->op == RECEIVE)
+    return(true);
+  if ((uses_global || uses_volatile) && (iic->op == CALL || iic->op == PCALL))
+    return(true);
+  if (uses_volatile && (POINTER_GET (iic) && IS_VOLATILE (operandType (left)->next)) || IS_OP_VOLATILE (left) || IS_OP_VOLATILE (right))
+    return(true);
+  if (uses_global && POINTER_SET (iic)) // TODO: More accuracy here!
+    return(true);
+
+  return(false);
+}
+
+static bool
+setup_cfg_for_expression (cfg_lospre_t *const cfg, const iCode *const eic)
+{
+  typedef boost::graph_traits<cfg_lospre_t>::vertex_descriptor vertex_t;
   bool safety_required = false;
 
   // In redundancy elimination, safety means not doing a computation on any path were it was not done before.
@@ -187,7 +210,7 @@ setup_cfg_for_expression (cfg_lospre_t *const cfg, const iCode *const eic)
     safety_required = true;
 
   // volatile requires safety.
-  if (uses_volatile)
+  if (POINTER_GET (eic) && IS_VOLATILE (operandType (IC_LEFT (eic))->next) || IS_OP_VOLATILE (IC_LEFT (eic)) || IS_OP_VOLATILE (IC_RIGHT (eic)))
     safety_required = true;
 
   // Reading from an invalid address might be dangerous, since there could be memory-mapped I/O.
@@ -210,23 +233,9 @@ setup_cfg_for_expression (cfg_lospre_t *const cfg, const iCode *const eic)
   for (vertex_t i = 0; i < boost::num_vertices (*cfg); i++)
     {
        const iCode *const ic = (*cfg)[i].ic;
-       const operand *const left = IC_LEFT (ic);
-       const operand *const right = IC_RIGHT (ic);
-       const operand *const result = IC_RESULT (ic);
 
        (*cfg)[i].uses = same_expression (eic, ic);
-       (*cfg)[i].invalidates = false;
-       if (IC_RESULT (ic) && !IS_OP_LITERAL (result) && !POINTER_SET(ic) &&
-         (eleft && isOperandEqual (eleft, result) || eright && isOperandEqual (eright, result)))
-         (*cfg)[i].invalidates = true;
-       if (ic->op == FUNCTION || ic->op == ENDFUNCTION || ic->op == RECEIVE)
-         (*cfg)[i].invalidates = true;
-       if ((uses_global || uses_volatile) && (ic->op == CALL || ic->op == PCALL))
-         (*cfg)[i].invalidates = true;
-       if (uses_volatile && (POINTER_GET (ic) && IS_VOLATILE (operandType (left)->next)) || IS_OP_VOLATILE (left) || IS_OP_VOLATILE (right))
-         (*cfg)[i].invalidates = true;
-       if (uses_global && POINTER_SET (ic)) // TODO: More accuracy here!
-         (*cfg)[i].invalidates = true;
+       (*cfg)[i].invalidates = invalidates_expression (eic, ic);
 
        (*cfg)[i].forward = std::pair<int, int>(-1, -1);
 
@@ -288,6 +297,158 @@ static void dump_dec_lospre(const tree_dec_t &tree_dec)
   delete[] name;
 }
 
+struct bbcfg_lospre_node
+{
+  bool uses;
+  bool invalidates;
+
+  iCode *firstic;
+};
+
+struct bbcfg_mcpre_node
+{
+  bool antloc, avloc, transp, kill;
+
+  iCode *firstic;
+};
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS, bbcfg_lospre_node, float> bbcfg_lospre_t; // The edge property is the cost of subdividing the edge and inserting an instruction (for now we always use 1, optimizing for code size, but relative execution frequency could be used when optimizing for speed or total energy consumption; aggregates thereof can be a good idea as well).
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS, bbcfg_mcpre_node, float> bbcfg_mcpre_t; // The edge property is the cost of subdividing the edge and inserting an instruction (for now we always use 1, optimizing for code size, but relative execution frequency could be used when optimizing for speed or total energy consumption; aggregates thereof can be a good idea as well).
+
+void
+create_bbcfg_lospre (bbcfg_lospre_t &cfg, iCode *start_ic, ebbIndex *ebbi)
+{
+  iCode *ic;
+
+  // Make a node for each basic block.
+  std::map<int, unsigned int> key_to_index;
+  {
+    int i = -1;
+    bool adding = false;
+
+    for (ic = start_ic; ic; ic = ic->next)
+      {
+        if (ic->op == LABEL)
+          adding = false;
+        if (!adding)
+        {
+          boost::add_vertex(cfg);
+          cfg[++i].firstic = ic;
+          adding = true;
+        }    
+        if (ic->op == IFX || ic->op == GOTO || ic->op == JUMPTABLE)
+          adding = false;
+
+        key_to_index[ic->key] = i;
+      }
+  }
+
+  // Get control flow graph from sdcc.
+  for (ic = start_ic; ic; ic = ic->next)
+    {
+      if((ic->op == '>' || ic->op == '<' || ic->op == LE_OP || ic->op == GE_OP || ic->op == EQ_OP || ic->op == NE_OP || ic->op == '^' || ic->op == '|' || ic->op == BITWISEAND) && ifxForOp (IC_RESULT (ic), ic) && key_to_index[ic->key] != key_to_index[ic->next->key])
+        boost::add_edge(key_to_index[ic->key], key_to_index[ic->next->key], 4.0f, cfg); // Try not to separate op from ifx.
+      else if (ic->op != GOTO && ic->op != RETURN && ic->op != JUMPTABLE && ic->next && key_to_index[ic->key] != key_to_index[ic->next->key])
+        boost::add_edge(key_to_index[ic->key], key_to_index[ic->next->key], 3.0f, cfg);
+
+      if (ic->op == GOTO)
+        boost::add_edge(key_to_index[ic->key], key_to_index[eBBWithEntryLabel(ebbi, ic->label)->sch->key], 6.0f, cfg);
+      else if (ic->op == RETURN)
+        boost::add_edge(key_to_index[ic->key], key_to_index[eBBWithEntryLabel(ebbi, returnLabel)->sch->key], 6.0f, cfg);
+      else if (ic->op == IFX)
+        boost::add_edge(key_to_index[ic->key], key_to_index[eBBWithEntryLabel(ebbi, IC_TRUE(ic) ? IC_TRUE(ic) : IC_FALSE(ic))->sch->key], 6.0f, cfg);
+      else if (ic->op == JUMPTABLE)
+        for (symbol *lbl = (symbol *)(setFirstItem (IC_JTLABELS (ic))); lbl; lbl = (symbol *)(setNextItem (IC_JTLABELS (ic))))
+          boost::add_edge(key_to_index[ic->key], key_to_index[eBBWithEntryLabel(ebbi, lbl)->sch->key], 6.0f, cfg);
+    }
+}
+
+void
+create_bbcfg_mcpre (bbcfg_mcpre_t &cfg, iCode *start_ic, ebbIndex *ebbi)
+{
+  iCode *ic;
+
+  // Make a node for each basic block.
+  std::map<int, unsigned int> key_to_index;
+  {
+    int i = -1;
+    bool adding = false;
+
+    for (ic = start_ic; ic; ic = ic->next)
+      {
+        if (ic->op == LABEL)
+          adding = false;
+        if (!adding)
+        {
+          boost::add_vertex(cfg);
+          cfg[++i].firstic = ic;
+          adding = true;
+        }    
+        if (ic->op == IFX || ic->op == GOTO || ic->op == JUMPTABLE)
+          adding = false;
+
+        key_to_index[ic->key] = i;
+      }
+  }
+
+  // Get control flow graph from sdcc.
+  for (ic = start_ic; ic; ic = ic->next)
+    {
+      if((ic->op == '>' || ic->op == '<' || ic->op == LE_OP || ic->op == GE_OP || ic->op == EQ_OP || ic->op == NE_OP || ic->op == '^' || ic->op == '|' || ic->op == BITWISEAND) && ifxForOp (IC_RESULT (ic), ic) && key_to_index[ic->key] != key_to_index[ic->next->key])
+        boost::add_edge(key_to_index[ic->key], key_to_index[ic->next->key], 4.0f, cfg); // Try not to separate op from ifx.
+      else if (ic->op != GOTO && ic->op != RETURN && ic->op != JUMPTABLE && ic->next && key_to_index[ic->key] != key_to_index[ic->next->key])
+        boost::add_edge(key_to_index[ic->key], key_to_index[ic->next->key], 3.0f, cfg);
+
+      if (ic->op == GOTO)
+        boost::add_edge(key_to_index[ic->key], key_to_index[eBBWithEntryLabel(ebbi, ic->label)->sch->key], 6.0f, cfg);
+      else if (ic->op == RETURN)
+        boost::add_edge(key_to_index[ic->key], key_to_index[eBBWithEntryLabel(ebbi, returnLabel)->sch->key], 6.0f, cfg);
+      else if (ic->op == IFX)
+        boost::add_edge(key_to_index[ic->key], key_to_index[eBBWithEntryLabel(ebbi, IC_TRUE(ic) ? IC_TRUE(ic) : IC_FALSE(ic))->sch->key], 6.0f, cfg);
+      else if (ic->op == JUMPTABLE)
+        for (symbol *lbl = (symbol *)(setFirstItem (IC_JTLABELS (ic))); lbl; lbl = (symbol *)(setNextItem (IC_JTLABELS (ic))))
+          boost::add_edge(key_to_index[ic->key], key_to_index[eBBWithEntryLabel(ebbi, lbl)->sch->key], 6.0f, cfg);
+    }
+}
+
+
+void dump_bbcfg_lospre (const bbcfg_lospre_t &cfg)
+{
+  if (!currFunc)
+    return;
+
+  std::ofstream dump_file((std::string(dstFileName) + ".dumplosprebbcfg" + currFunc->rname + ".dot").c_str());
+
+  std::string *name = new std::string[num_vertices(cfg)];
+  for (unsigned int i = 0; i < boost::num_vertices(cfg); i++)
+    {
+      std::ostringstream os;
+      os << "First ic key " << cfg[i].firstic->key;
+      name[i] = os.str();
+    }
+  boost::write_graphviz(dump_file, cfg, boost::make_label_writer(name), boost::default_writer(), cfg_titlewriter(currFunc->rname, "bb-lospre"));
+  delete[] name;
+}
+
+void dump_bbcfg_mcpre (const bbcfg_mcpre_t &cfg)
+{
+  if (!currFunc)
+    return;
+
+  std::ofstream dump_file((std::string(dstFileName) + ".dumpmcprebbcfg" + currFunc->rname + ".dot").c_str());
+
+  std::string *name = new std::string[num_vertices(cfg)];
+  for (unsigned int i = 0; i < boost::num_vertices(cfg); i++)
+    {
+      std::ostringstream os;
+      os << "First ic key " << cfg[i].firstic->key;
+      name[i] = os.str();
+    }
+  boost::write_graphviz(dump_file, cfg, boost::make_label_writer(name), boost::default_writer(), cfg_titlewriter(currFunc->rname, "bb-mcpre"));
+  delete[] name;
+}
+
 void
 lospre (iCode *sic, ebbIndex *ebbi)
 {
@@ -311,6 +472,23 @@ lospre (iCode *sic, ebbIndex *ebbi)
   if(options.dump_graphs)
     dump_dec_lospre(tree_decomposition);
 
+  {
+    bbcfg_lospre_t control_flow_graph;
+
+    create_bbcfg_lospre (control_flow_graph, sic, ebbi);
+
+    dump_bbcfg_lospre(control_flow_graph);
+  }
+
+  {
+    bbcfg_mcpre_t control_flow_graph;
+
+    create_bbcfg_mcpre (control_flow_graph, sic, ebbi);
+
+    dump_bbcfg_mcpre(control_flow_graph);
+  }
+
+#if 1
   int lkey = operandKey;
 
   for (bool change = true; change;)
@@ -340,5 +518,6 @@ lospre (iCode *sic, ebbIndex *ebbi)
           change |= (tree_dec_lospre (tree_decomposition, control_flow_graph, ic) > 0);
         }
     }
+#endif
 }
 

3f890aa11c2b395ba26f46576b21bdc41e97e681
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2019-10-02 08:57:27 +0000

    Remove support for idxsp, add pdk15 support for spadd.
    git-svn-id: svn://svn.code.sf.net/p/sdcc/code/branches/pdk@11414 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/sdas/aspdk13/pdk13mch.c
+++ sdcc/sdas/aspdk13/pdk13mch.c
@@ -242,7 +242,6 @@ machine(struct mne *mp)
         case S_LDSPTL: /* undocumented */
         case S_LDSPTH: /* undocumented */
 
-        case S_IDXSP: /* proposed */
         case S_SPADD: /* proposed */
 
                 outaw(op);

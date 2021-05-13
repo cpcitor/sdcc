50d61e64cd336e0ca2fccc2261ce3644ba69221e
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2021-05-13 12:40:04 +0000

    Merge from trunk.
    git-svn-id: svn://svn.code.sf.net/p/sdcc/code/branches/noasm2@12352 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/src/z80/peep.c
+++ sdcc/src/z80/peep.c
@@ -182,10 +182,12 @@ z80MightBeParmInCallFromCurrentFunction(const char *what)
     return TRUE;
   if (strchr(what, 'b') && z80_regs_used_as_parms_in_calls_from_current_function[B_IDX])
     return TRUE;
+  if (strchr(what, 'a') && z80_regs_used_as_parms_in_calls_from_current_function[A_IDX])
+    return true;
   if (strstr(what, "iy") && (z80_regs_used_as_parms_in_calls_from_current_function[IYL_IDX] || z80_regs_used_as_parms_in_calls_from_current_function[IYH_IDX]))
-    return TRUE;
+    return true;
 
-  return FALSE;
+  return false;
 }
 
 /* Check if the flag implies reading what. */
@@ -357,8 +359,8 @@ z80MightRead(const lineNode *pl, const char *what)
   if(ISINST(pl->line, "reti") || ISINST(pl->line, "retn"))
     return(strcmp(what, "sp") == 0);
 
-  if(ISINST(pl->line, "ret")) // --reserve-regs-iy uses ret in code gen for calls through function pointers
-    return(IY_RESERVED ? z80IsReturned(what) || z80MightBeParmInCallFromCurrentFunction(what) : z80IsReturned(what)) || strcmp(what, "sp") == 0;
+  if(ISINST(pl->line, "ret")) // --reserve-regs-iy and the gbz80 port use ret in code gen for calls through function pointers
+    return((IY_RESERVED || IS_GB) ? z80IsReturned(what) || z80MightBeParmInCallFromCurrentFunction(what) : z80IsReturned(what)) || strcmp(what, "sp") == 0;
 
   if(!strcmp(pl->line, "ex\t(sp), hl") || !strcmp(pl->line, "ex\t(sp),hl"))
     return(!strcmp(what, "h") || !strcmp(what, "l") || strcmp(what, "sp") == 0);
@@ -1326,12 +1328,21 @@ int z80instructionSize(lineNode *pl)
       if(IS_RAB && !STRNCASECMP(op1start, "hl", 2) && (argCont(op2start, "(sp)") || argCont(op2start, "(ix)")))
         return(3);
 
-      if(IS_EZ80_Z80 && /* eZ80 16-bit pointer load */
+      /* eZ80 16-bit pointer load */
+      if(IS_EZ80_Z80 &&
         (!STRNCASECMP(op1start, "bc", 2) || !STRNCASECMP(op1start, "de", 2) || !STRNCASECMP(op1start, "hl", 2) || !STRNCASECMP(op1start, "ix", 2) || !STRNCASECMP(op1start, "iy", 2)))
         {
           if (!STRNCASECMP(op2start, "(hl)", 4))
             return(2);
           if (argCont(op2start, "(ix)") || argCont(op2start, "(iy)"))
+            return(3);
+        }
+      if(IS_EZ80_Z80 &&
+        (!STRNCASECMP(op2start, "bc", 2) || !STRNCASECMP(op2start, "de", 2) || !STRNCASECMP(op2start, "hl", 2) || !STRNCASECMP(op2start, "ix", 2) || !STRNCASECMP(op2start, "iy", 2)))
+        {
+          if (!STRNCASECMP(op1start, "(hl)", 4))
+            return(2);
+          if (argCont(op1start, "(ix)") || argCont(op1start, "(iy)"))
             return(3);
         }
 

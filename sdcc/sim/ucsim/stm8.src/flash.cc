604a630752e764a39382590cd4b606d24688bf7a
spth <spth@4a8a32a2-be11-0410-ad9d-d568d2c75423>
2021-11-04 09:15:12 +0000

    Merge from trunk.
    git-svn-id: svn://svn.code.sf.net/p/sdcc/code/branches/f8@12756 4a8a32a2-be11-0410-ad9d-d568d2c75423

--- sdcc/sim/ucsim/stm8.src/flash.cc
+++ sdcc/sim/ucsim/stm8.src/flash.cc
@@ -66,11 +66,11 @@ cl_flash_as::cl_flash_as(const char *id, t_addr astart, t_addr asize):
   decoders= new cl_decoder_list(2, 2, false);
   cella= (class cl_memory_cell *)malloc(size * sizeof(class cl_memory_cell));
   //cell->init();
-  int i;
+  t_addr i;
   for (i= 0; i < size; i++)
     {
       void *p= &(cella[i]);
-      memcpy(p, cell, sizeof(class cl_memory_cell));
+      memcpy(p, (void*)cell, sizeof(class cl_memory_cell));
       cella[i].init();
     }
   dummy= new cl_dummy_cell(8);
